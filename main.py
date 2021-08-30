440cbe6e2d4eb16f566a64a198f76a8521798626
emoji3090 <emoji3090@gmail.com>
2021-08-30 23:32:57 +0430

    Menu

---
+++ main.py
@@ -0,0 +1,17 @@
+
+if __name__ == '__main__':
+
+    print("Choose one of these options : ")
+    print("  1 - Start")
+    print("  2 - Exit")
+
+    choice = int(input("Enter Options Num : "))
+
+    if choice == 1:
+        print("Start")
+
+    elif choice == 2:
+        exit()
+
+    else:
+        print("Wrong Option's Num")
