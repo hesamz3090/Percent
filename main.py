975353c5af951aa32ed8ef8998fe4294024fe630
emoji3090 <emoji3090@gmail.com>
2021-09-01 22:23:08 +0430

    Start Part 2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from config import *
+import random
 
+from config import *
+from binance import Client
 
 if __name__ == '__main__':
 
@@ -10,22 +12,68 @@ if __name__ == '__main__':
     choice = int(input("Enter Options Num : "))
 
     if choice == 1:
-        for key, value in symbols.items():
-            print(f"  {value} - {key}")
+        if profile["key_one"] != "" \
+                and profile["secret_one"] != "" \
+                and profile["key_two"] != "" \
+                and profile["secret_two"] != "":
+            for key, value in symbols.items():
+                print(f"  {value} - {key}")
 
-        symbol_num = int(input("Enter Symbol Num : "))
+            symbol_num = int(input("Enter Symbol Num : "))
 
-        symbol = ""
-        for key, value in symbols.items():
-            if value == symbol_num:
-                symbol = key
+            symbol = ""
+            for key, value in symbols.items():
+                if value == symbol_num:
+                    symbol = key
 
-        if symbol != "":
-            quantity = int(input("Enter Quantity : "))
-            print(f'trade {quantity} - {symbol}')
+            if symbol != "":
+                quantity = int(input("Enter Quantity : "))
 
+                client_one = Client(profile.binance_api_key, profile.binance_api_secret)
+                client_two = Client(profile.binance_api_key, profile.binance_api_secret)
+
+                num = random.randint(1, 2)
+
+                if num == 1:
+                    side = "BUY"
+                    un_side = "SELL"
+
+                else:
+                    side = "SELL"
+                    un_side = "BUY"
+
+                result_one = client_one.futures_create_order(
+                    symbol=symbol,
+                    side=side,
+                    type='MARKET',
+                    quantity=quantity,
+                )
+
+                result_two = client_two.futures_create_order(
+                    symbol=symbol,
+                    side=side,
+                    type='MARKET',
+                    quantity=quantity,
+                )
+
+                trailing_one = client_one.futures_create_order(
+                    symbol=symbol,
+                    type='TRAILING_STOP_MARKET',
+                    side=un_side,
+                    callbackRate=5
+                )
+
+                trailing_two = client_two.futures_create_order(
+                    symbol=symbol,
+                    type='TRAILING_STOP_MARKET',
+                    side=un_side,
+                    callbackRate=5
+                )
+
+            else:
+                print("Wrong Symbol's Num")
         else:
-            print("Wrong Symbol's Num")
+            print("Please Edit Config.py")
 
     elif choice == 2:
         exit()
